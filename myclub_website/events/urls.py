28ccc136381e9f1624e4c2a05eb816bf3805ccea
Gaurund <pavel.makushev@gmail.com>
2024-06-10 14:51:03 +0300

    List of venues done

--- myclub_website/events/urls.py
+++ myclub_website/events/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('<int:year>/<str:month>', views.home, name='home'),
     path('events', views.all_events, name='list-events'),
-    path('add_venue', views.add_venue, name='add-venue')
+    path('add_venue', views.add_venue, name='add-venue'),
+    path('list_venues', views.list_venues, name='list-venues'),
+    path('show_venue/<venue_id>', views.show_venue, name='show-venue'),
 ] + static(
     settings.STATIC_URL,
     document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
